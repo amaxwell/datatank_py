f39d97d20b176bd5549714288643f04ee5d441c3
Adam R. Maxwell <amaxwell@mac.com>
2014-02-24 19:18:14 -0800

    add __all__ for import *

--- datatank_py/__init__.py
+++ datatank_py/__init__.py
@@ -1,1 +1,7 @@
+#!/usr/bin/env python
+# coding: utf-8
 
+# from glob import glob
+# [x.strip(".py") for x in glob("*.py")]
+
+__all__ = ['DTBitmap2D', 'DTDataFile', 'DTError', 'DTMask', 'DTMesh2D', 'DTPath2D', 'DTPathValues2D', 'DTPlot1D', 'DTPoint2D', 'DTPointCollection2D', 'DTPointValue2D', 'DTPointValueCollection2D', 'DTProgress', 'DTPyCoreImage', 'DTPyWrite', 'DTRegion2D', 'DTRegion3D', 'DTSeries', 'DTStructuredGrid2D', 'DTStructuredGrid3D', 'DTStructuredMesh2D', 'DTStructuredMesh3D', 'DTStructuredVectorField2D', 'DTStructuredVectorField3D', 'DTTriangularGrid2D', 'DTTriangularMesh2D', 'DTTriangularVectorField2D', 'DTVector2D']
