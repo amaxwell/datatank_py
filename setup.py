85dc16f972b94be1b7f5384296dcc43cbaf4184e
Adam R. Maxwell <amaxwell@mac.com>
2014-03-11 21:47:33 -0700

    start adding pypi stuff

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+#
+# http://peterdowns.com/posts/first-time-with-pypi.html
+#
 from setuptools import setup, find_packages
 setup(
     name = "datatank_py",
@@ -5,12 +8,12 @@ setup(
     packages = find_packages(),
     
     install_requires = ['numpy>1.0'],
-    # package_data = {
-    #     "" : [ "*.tank", "*.markdown", "*.txt" ]
-    # },
-    
+    zip_safe = False
     author = "Adam R. Maxwell",
     author_email = "amaxwell@mac.com",
     description = "Python modules for creating and modifying DataTank files",
-    license = "BSD",
+    license = "BSD New",
+    url = "http://github.com/amaxwell/datatank_py",
+    doc_url = "http://amaxwell.github.io/datatank_py",
+    download_url = "http://github.com/amaxwell/datatank_py/tarball/0.1",
 )
