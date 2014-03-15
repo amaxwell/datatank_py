e8947bdad25d5a3cc55058e1025847d66835d710
Adam R. Maxwell <amaxwell@mac.com>
2014-03-14 20:36:45 -0700

    rename README, add better description, remove unused key

--- setup.py
+++ setup.py
@@ -11,9 +11,23 @@ setup(
     zip_safe = False,
     author = "Adam R. Maxwell",
     author_email = "amaxwell@mac.com",
-    description = "Python modules for creating and modifying DataTank files",
+    description = """
+        =============
+        WHAT IS THIS?
+        =============
+
+        This is a Python module that allows you to read and write DataTank data files using
+        Python. `DataTank <http://visualdatatools.com/DataTank/>`_ is a visualization and 
+        analysis tool for Mac OS X, but datatank_py is cross-platform.
+        
+        `Documentation <http://amaxwell.github.io/datatank_py>`_
+
+        Although DataTank itself is proprietary (now free of
+        charge for students and postdocs), it includes open-source C++ libraries for most
+        of the internal data structures, so you can easily create data files to be loaded
+        into it. The datatank_py module is a reimplementation of the C++ libraries, so
+        tends to be more Pythonic.""",
     license = "BSD New",
     url = "http://github.com/amaxwell/datatank_py",
-    doc_url = "http://amaxwell.github.io/datatank_py",
     download_url = "http://github.com/amaxwell/datatank_py/tarball/0.4",
 )
