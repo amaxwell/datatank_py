61726ca36599f8e46762686f099367da18a21223
Adam R. Maxwell <amaxwell@mac.com>
2014-03-18 09:03:12 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,32 +2,35 @@
 # http://peterdowns.com/posts/first-time-with-pypi.html
 #
 from setuptools import setup, find_packages
+
+# http://docs.python.org/2.7/distutils/packageindex.html#the-pypirc-file
+with open("README.rst") as readme:
+    long_description = readme.read()
+
 setup(
     name = "datatank_py",
-    version = "0.5",
+    version = "0.6",
     packages = find_packages(),
     
     install_requires = ['numpy>1.0'],
     zip_safe = False,
     author = "Adam R. Maxwell",
     author_email = "amaxwell@mac.com",
-    description = """
-        =============
-        WHAT IS THIS?
-        =============
-
-        This is a Python module that allows you to read and write DataTank data files using
-        Python. `DataTank <http://visualdatatools.com/DataTank/>`_ is a visualization and 
-        analysis tool for Mac OS X, but datatank_py is cross-platform.
-        
-        `Documentation <http://amaxwell.github.io/datatank_py>`_
-
-        Although DataTank itself is proprietary (now free of
-        charge for students and postdocs), it includes open-source C++ libraries for most
-        of the internal data structures, so you can easily create data files to be loaded
-        into it. The datatank_py module is a reimplementation of the C++ libraries, so
-        tends to be more Pythonic.""",
+    description = "Module for reading and writing DataTank files",
+    long_description = long_description,
     license = "BSD New",
     url = "http://github.com/amaxwell/datatank_py",
-    download_url = "http://github.com/amaxwell/datatank_py/tarball/0.4",
+    download_url = "http://github.com/amaxwell/datatank_py/tarball/0.6",
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft",
+        "Operating System :: POSIX",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
 )
