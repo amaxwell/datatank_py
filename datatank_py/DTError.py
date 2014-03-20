b5e7ad16c8e9c56c7384bc5375e049643381ad5c
Adam R. Maxwell <amaxwell@mac.com>
2014-03-20 11:17:11 -0700

    improve error logging API

--- datatank_py/DTError.py
+++ datatank_py/DTError.py
@@ -3,12 +3,41 @@
 
 # This software is under a BSD license.  See LICENSE.txt for details.
 
-__all__ = ["DTErrorMessage", "DTSaveError"]
+__all__ = ["DTErrorMessage", "DTSaveError", "dt_set_log_identifier", "dt_use_syslog"]
 
 import sys
 import os
+from syslog import syslog, openlog, LOG_ERR
 
 _errors = []
+_DEFAULT_CONTEXT = os.path.basename(sys.argv[0])
+_USE_SYSLOG = False
+
+def dt_set_log_identifier(ctxt):
+    """Sets the default logging identifier to something useful.
+    
+    :param ctxt: a string that will usefully identify this log message
+    
+    Typically you should just pass in the basename of your Python script here.
+    Since DataTank changes executable names in modules (and perhaps other times),
+    the default of ``sys.argv[0]`` isn't effective in tracking down
+    error messages, since they're all attributed to "runme" or something similar.
+    
+    """
+    
+    global _DEFAULT_CONTEXT
+    _DEFAULT_CONTEXT = ctxt
+    openlog(ident=ctxt)
+    
+def dt_use_syslog(should_use):
+    """Allows you to copy all messages to syslog.
+    
+    :param should_use: pass ``True`` to use syslog
+    
+    """
+    
+    global _USE_SYSLOG
+    _USE_SYSLOG = should_use
 
 def DTErrorMessage(fcn, msg):
     """Accumulate a message and echo to standard error.
@@ -22,13 +51,23 @@ def DTErrorMessage(fcn, msg):
     usage.
     
     """
-    
+        
     if fcn == None:
-        fcn = os.path.basename(sys.argv[0])
+        fcn = _DEFAULT_CONTEXT
     
     err_msg = "%s: %s" % (fcn, msg)
     _errors.append(err_msg)
+    
+    # this is harmless, and lets you get errors when running in console mode
     sys.stderr.write(err_msg + "\n")
+    
+    # syslog will do its own ident string if we haven't set it explicitly
+    if _USE_SYSLOG:
+        syslog(LOG_ERR, msg)
+    
+def DTWarningMessage(fcn, msg):
+    """Calls :func:`DTErrorMessage`, which is what C++ DTSource does."""
+    DTErrorMessage(fcn, msg)
 
 def DTSaveError(datafile, name="ExecutionErrors"):
     """Save accumulated messages to a file.
